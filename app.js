9952951beb9153f6088270f58c175a364b8b6c8e
t-cool <hiroki802@hotmail.com>
2020-01-30 20:31:21 +0900

    update

--- app.js
+++ app.js
@@ -2,8 +2,23 @@ var btn = document.getElementById('btn');
 //var content = document.getElementById('content');
 var textarea = document.getElementById('textarea1');
 
+
+SpeechRecognition = webkitSpeechRecognition || SpeechRecognition;
+const recognition = new SpeechRecognition();
+
+recognition.continuous = true; // これこれ
+
+recognition.onresult = (event) => {
+  console.log(event.results);
+}
+
+recognition.start();
+
+/*
 //音声認識APIの使用
-var speech = new webkitSpeechRecognition();
+SpeechRecognition = webkitSpeechRecognition || SpeechRecognition;
+const speech = new SpeechRecognition();
+speech.interimResults = true;
 
 //言語を日本語に設定
 speech.lang = "en";
@@ -20,66 +35,4 @@ speech.addEventListener('result', function(e) {
   textarea.value += text + '\n';
   console.log(text);
 });
-
-
-//検索
-function getSearch() {
-    var iframe = document.createElement('iframe');
-    
-    iframe.width = '400px';
-    iframe.height = '400px';
-    iframe.src = 'https://duckduckgo.com/';
-    content.appendChild(iframe);
-}
-
-//ビデオ
-function getVideo() {
-    var URL = '<iframe width="560" height="315" src="https://www.youtube.com/embed/TBEuMfNqv_k?rel=0&amp;controls=0&amp;showinfo=0;autoplay=1" frameborder="0" allowfullscreen></iframe>';
-    
-    content.innerHTML = URL;
-}
-
-//ラジオ
-function getRadio() {
-  var URL = '<iframe src="http://tunein.com/embed/player/s48940" style="width:50%;height:100px;" scrolling="no" frameborder="no"></iframe>';
-  
-  content.innerHTML = URL;
-}
-
-//イベント
-function getEventData() {
-  // キーワードを「東京」に設定
-  var baseURL = "https://api.atnd.org/events/?keyword=東京&format=jsonp&count=20&callback=callback";
-  var script = document.createElement('script');
-  
-  script.src = baseURL;
-  document.body.appendChild(script);
-  
-  
-  window.callback = function(data) {
-    var ul = document.createElement('ul');
-    content.appendChild(ul);
-    
-    for(var item in data.events) {
-      var li = document.createElement('li');
-      var a = document.createElement('a');
-      
-      a.href = data.events[item].event.event_url;
-      a.textContent = data.events[item].event.title;
-      a.target = "_blank";
-      li.style.lineHeight = 1.5;
-      li.appendChild(a);
-      ul.appendChild(li);
-    }
-    
-  };
-}
-
-//テキスト表示
-function getTextContents(text) {
-  content.innerHTML = '<p>認識された言葉</p>' +
-                   '<input type="text" value="' + text + '">';
-}
-
-
-
+*/
